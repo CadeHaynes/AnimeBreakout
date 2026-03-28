9255e8ab8db53d5c1d55b19e331cfa206cf4c474
Laevahteinn <cade.haynes@gmail.com>
2026-03-28 12:35:34 +1100

    Partial progress on BallManager, can activate and instantiate balls (not tested)

---
+++ AnimeBreakout/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,18 @@
+using Game.Objects.Block;
+using UnityEngine;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        /*
+         * Game manager should do the following:
+         * - Store and control all balls
+         * - Store and control player info
+         * - Manage pause menus
+         */
+
+        BlockManager _blockManager;
+
+    }
+}
