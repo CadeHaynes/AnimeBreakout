63e64519fedf5ec3fdbe710621bb4d87f8b07893
Laevahteinn <cade.haynes@gmail.com>
2026-06-27 15:44:20 +1000

    Change to prevent character from clinging to walls.

--- AnimeBreakout/Assets/Scripts/Character/CharacterGround.cs
+++ AnimeBreakout/Assets/Scripts/Character/CharacterGround.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Game.Character
@@ -9,6 +10,8 @@ namespace Game.Character
 
         [SerializeField] int _airborneGravityScale = 5;
 
+        [SerializeField] float _xBoundary = 0.1f;
+
         [SerializeField] PhysicsMaterial2D _baseMaterial;
         [SerializeField] PhysicsMaterial2D _airborneMaterial;
 
@@ -16,7 +19,11 @@ namespace Game.Character
 
         public bool IsGrounded()
         {
-            bool isGrounded = Physics2D.BoxCast(transform.position, transform.localScale, 0f, Vector2.down, 0.2f, _groundLayers);
+            if (!_rb) return false;
+
+            Vector2 boundScale = new Vector2(transform.localScale.x - _xBoundary, transform.localScale.y);
+
+            bool isGrounded = Physics2D.BoxCast(transform.position, boundScale, 0f, Vector2.down, 0.2f, _groundLayers);
 
             return isGrounded;
         }
